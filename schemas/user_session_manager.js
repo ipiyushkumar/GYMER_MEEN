f745f739029bdef896043ed5f92ef144520e3116
dark-king-001 <piyushat115@gmail.com>
2024-03-15 10:56:57 +0530

    added client session Id

--- schemas/user_session_manager.js
+++ schemas/user_session_manager.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSessionTrack = new mongoose.Schema({
   sessionLandingUrl: { type: Number, default: "https://whitewolfIndia.com" },
+  sessionId: { type: String }, // client session id
   visited: { type: Number, default: 0 }, // if the website was visited
   addedToCart: { type: Number, default: 0 }, // if any item was added to cart
   reachedCheckout: { type: Number, default: 0 }, // if user opened the checkout
